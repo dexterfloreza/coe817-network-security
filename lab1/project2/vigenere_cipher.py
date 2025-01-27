005a50f7232f25026afc89e79d4073a293a8c5a1
dexterfloreza <deryanfloreza@outlook.com>
2025-01-26 20:55:46 -0500

    Fixed the vigenere cipher lowercase uppercase sensitivity issue

--- lab1/project2/vigenere_cipher.py
+++ lab1/project2/vigenere_cipher.py
@@ -1,11 +1,16 @@
 def generate_key(msg, key):
     key = list(key)
-    if len(msg) == len(key):
-        return key
-    else:
-        for i in range(len(msg) - len(key)):
-            key.append(key[i % len(key)])
-    return "".join(key)
+    full_key = []
+    key_index = 0
+
+    for char in msg:
+        if char.isalpha():  # Only extend the key for alphabetic characters
+            full_key.append(key[key_index % len(key)])
+            key_index += 1
+        else:
+            full_key.append(char)  # Retain non-alphabetic characters as-is
+
+    return "".join(full_key)
 
 class VigenereCipher:
     def __init__(self, key):
@@ -13,11 +18,11 @@ class VigenereCipher:
 
     def encrypt(self, msg):
         encrypted_text = []
-        key = generate_key(msg, self.key)
+        key = generate_key(msg, self.key.lower())  # Normalize key to lowercase
         for i in range(len(msg)):
             char = msg[i]
             if char.isupper():
-                encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
+                encrypted_char = chr((ord(char) + ord(key[i].upper()) - 2 * ord('A')) % 26 + ord('A'))
             elif char.islower():
                 encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))
             else:
@@ -27,11 +32,11 @@ class VigenereCipher:
 
     def decrypt(self, msg):
         decrypted_text = []
-        key = generate_key(msg, self.key)
+        key = generate_key(msg, self.key.lower())  # Normalize key to lowercase
         for i in range(len(msg)):
             char = msg[i]
             if char.isupper():
-                decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
+                decrypted_char = chr((ord(char) - ord(key[i].upper()) + 26) % 26 + ord('A'))
             elif char.islower():
                 decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
             else:
