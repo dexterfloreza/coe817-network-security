340d83e44ed7963a8ffa859d949dbf259f9a16b5
vishnupan819 <vishnupan819@gmail.com>
2025-02-10 14:48:33 -0500

    Changes committed

--- lab2/project2/alice_client.py
+++ lab2/project2/alice_client.py
@@ -22,24 +22,24 @@ class alice_client:
                 # Sending Alice's public key and nonce info to Bob
                 data = pickle.dumps((self.public_key_obj.serialize_publicKey(), alice_nonce))
                 client.sendall(data)
-                # print("Alice sent her public key and nonce to Bob. ")
+                print("Alice sent her public key and nonce to Bob. ")
                 
                 # Receive Bob's public key from Bob
                 bob_public_key_bytes = client.recv(4096)
                 bob_public_key = self.public_key_obj.deserialize_publicKey(bob_public_key_bytes)
-                # print("Alice recieved Bob's public key. ")
+                print("Alice recieved Bob's public key. ")
                 
                 # Get encrypted message from Bob
                 encrypted_msg = client.recv(4096)
-                print("the recieved message 2")
+                # print("the recieved message 2")
                 decrypted_msg = self.public_key_obj.decrypt_privateKey(encrypted_msg)
-                print("the decrypted message 2")
+                # print("the decrypted message 2")
                 received_NA, bob_nonce = decrypted_msg[:16], decrypted_msg[16:]
-                # print(f"Alice received: NA = {received_NA}, NB = {bob_nonce}")
+                print(f"Alice received: NA = {received_NA}, NB = {bob_nonce}")
 
                 # Verify Alice's nonce
                 if received_NA == alice_nonce:
-                    print("the received message 3")
+                    print("Alice's nonce is verified.")
                 else:
                     print("Nonce verification failed.")
                     return # Exit if verification fails
